a327391c7ae05013a7fd417096ceffab6c9b2e1f
rdecker7 <rdecker7@mail.csuchico.edu>
2017-10-10 14:44:25 -0700

    This is the final version of the lab

--- mult.cpp
+++ mult.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 int main(void)
 {
-vector< vector<int> > table(12);
+vector< vector<int> > table(13);
 
-  for(int row=0;row<table.size();row++)
+  for(int row=1;row<table.size();row++)
   {
-  table[row].resize(12);
-    
-    for(int col=0;col<table[row].size();col++)
+  table[row].resize(13);
+    for(int col=1;col<table[row].size();col++)
     {
-    
+    table[row][col]=row*col;
+    cout<<table[row][col]<<" ";
     }
+  cout<<endl;
   }
   
-  for( int row=0;row<table.size();++row)
+/*  for( int row=0;row<table.size();++row)
   {
-  table.push_back(row+1)<<endl;
     for(int col=0;col<table[row].size();++col)
     {
-    table[row].push_back(col+1)<<endl;
+    table[row][col]=col+1;
     cout<<table[row][col]<<endl;
     }
-  }
+  }*/
 return 0;
 }
